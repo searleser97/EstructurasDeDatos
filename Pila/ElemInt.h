9c952875e1fbfd126a6064642544319e4dfba309
san <serchgabriel97@gmail.om>
2017-03-21 21:16:32 -0600

    erasing

--- Pila/ElemInt.h
+++
@@ -1,7 +0,0 @@
-typedef int Elem;
-
-using namespace std;
-
-void impElem (Elem e){cout << e << endl;}
-int igualesElem (Elem e1,Elem e2){return e1==e2;}
-int esMenor (Elem e1,Elem e2){return e1<e2;}

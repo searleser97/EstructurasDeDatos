bf9632194883f694376edee188d5524d6b401bae
san <serchgabriel97@gmail.om>
2017-03-23 17:41:38 -0600

    ColaCircular

--- ColaCircularLista/Elem.h
+++
@@ -1,9 +0,0 @@
-#include <cstdlib>
-#include <ctime>
-#include <cstdio>
-
-#include <iostream>
-
-typedef char Elem;
-
-using namespace std;
--- Lista/ElemStr.h
+++
@@ -1,8 +0,0 @@
-#include <cstring>
-typedef char* Elem;
-
-using namespace std;
-
-void impElem (Elem e) {cout << e << endl;}
-int igualesElem (Elem e1, Elem e2) {return strcmp(e1, e2) == 0;}
-int esMenor (Elem e1, Elem e2) {return strcmp(e1, e2) < 0;}

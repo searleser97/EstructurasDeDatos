19cc7588d15bbace6676ab9f8a9a0e1d9f295d49
searleser97 <serchgabriel97@gmail.com>
2017-04-07 11:08:06 -0500

    BinaryTree(size)

--- ArbolBinarioCpp/BinaryTree.cpp
+++ ArbolBinarioCpp/BinaryTree.cpp
@@ -10,19 +10,39 @@ typedef char Elem;
 
 // Binary tree structure
 template <class T> class Node {
-	
-	public: T data;
-	public: Node<T>* left;
-	public: Node<T>* right;
+
+public: T data;
+public: Node<T>* left;
+public: Node<T>* right;
+// public: int size;
 
 	Node (T data, Node<T>* left, Node<T>* right) {
 		this -> data = data;
 		this -> left = left;
 		this -> right = right;
 	}
+
+	public: bool isEmpty() {
+		if (this == NULL)
+			return true;
+		else
+			return false;
+	}
+
+	public: int size () {
+		return _size(this);
+	}
+
+	private: int _size(Node<T>* root) {
+		if (root == NULL)
+			return 0;
+		else
+			return 1 + _size(root -> left) + _size(root -> right);
+	}
+
 };
 
-Elem random();
+Elem randoms();
 Node<Elem>* createNode(Elem e);
 void randomSons(Node<Elem>* root, int niveles);
 Node<Elem>* createRandomTree(int niveles);
@@ -33,7 +53,13 @@ void inorder(Node<Elem>* root);
 int main () {
 	srand(time(NULL));
 
-	Node<Elem>* root = createNode('F');
+	Node<Elem>* root = NULL;
+
+	cout << endl;
+	cout << "isEmpty() = " << (root -> isEmpty() ? "True" : "False") << endl;
+	cout << endl;
+
+	root = createNode('F');
 	root -> left = createNode('B');
 	root -> left -> left = createNode('A');
 	root -> left -> right = createNode('D');
@@ -55,12 +81,16 @@ int main () {
 	inorder(root);
 	cout << endl << endl;
 
+	cout << "isEmpty() = " << (root -> isEmpty() ? "True" : "False") << endl;
+	cout << endl;
+	cout << "size() = " << root -> size() << endl;
+
 	return 0;
 }
 
 // inclusive random
-Elem random() {
-	
+Elem randoms() {
+
 	int min = 33;
 	int max = 126;
 	char ran = rand() % (max + 1 - min) + min;
@@ -78,14 +108,14 @@ void randomSons(Node<Elem>* root, int niveles) {
 		return;
 	}
 
-	Node<Elem>* left = createNode(random());
-	Node<Elem>* right = createNode(random());
+	Node<Elem>* left = createNode(randoms());
+	Node<Elem>* right = createNode(randoms());
 	root -> left = left;
 	root -> right = right;
 	niveles--;
 	randomSons(root -> left, niveles);
 	randomSons(root -> right, niveles);
-	
+
 }
 
 Node<Elem>* createRandomTree(int niveles) {

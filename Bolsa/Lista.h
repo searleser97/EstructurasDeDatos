ad56a166aef37b92da4f4d4c21a7b675ca528836
searleser97 <serchgabriel97@gmail.com>
2017-04-17 07:31:55 -0500

    Bolsa

---
+++ Bolsa/Lista.h
@@ -0,0 +1,73 @@
+#include "ElemInt.h"
+#include <cstdlib>
+#include <ctime>
+
+typedef struct Nodo
+{
+	Elem dato;
+	struct Nodo *sig;
+} *Lista;
+
+int esvacia(Lista l) {return l == NULL;}
+
+Elem cabeza(Lista l) {return l->dato;}
+
+Lista resto(Lista l) {return l->sig;}
+Lista vacia() {return NULL;}
+
+Lista cons(Elem e, Lista l)
+{
+	Lista t = (Lista)malloc(sizeof(struct Nodo));
+	t->dato = e;
+	t->sig = l;
+	return t;
+}
+
+int numElem(Lista l) {
+	if(esvacia(l)) return 0;
+	else return 1 + numElem(resto(l));
+}
+
+void impLista(Lista l) {
+	if (l != NULL) {
+		impElem(cabeza(l));
+		impLista(resto(l));
+	}
+}
+
+Lista pegaListas(Lista l1, Lista l2) {
+	if (esvacia(l1)) return l2;
+	else cons(cabeza(l1), pegaListas(resto(l1),l2));
+}
+
+Lista invierteLista(Lista l){
+	if (esvacia(l)) return l;
+	else return pegaListas(invierteLista(resto(l)),cons(cabeza(l), vacia()));
+}
+
+int sonIguales(Lista l1, Lista l2) {
+	if (esvacia(l1) && esvacia(l2)) return 1;
+	else if (esvacia(l1) && !esvacia(l2)) return 0;
+	else if (!esvacia(l1) && esvacia(l2)) return 0;
+	else return igualesElem(cabeza(l1), cabeza(l2)) && sonIguales(resto(l1), resto(l2));
+}
+
+int palindroma(Lista l) {return sonIguales(l, invierteLista(l));}
+
+
+Lista insOrden(Elem e, Lista l) {
+	if(esvacia(l)) return cons(e, vacia());
+	else if (esMenor(e, cabeza(l))) return cons(e, l);
+	else return cons(cabeza(l), insOrden(e, resto(l)));
+}
+
+Lista ordena(Lista l) {
+	if(esvacia(l)) return l;
+	else return insOrden(cabeza(l), ordena(resto(l)));
+}
+
+Lista consListaInt(int size, int from, int to) {
+	if (size == 0) return vacia();
+	int randN = rand() % to + from;
+	return cons((Elem)randN, consListaInt(size - 1, from, to));
+}
---
+++ Bolsa/conjunto.h
@@ -0,0 +1,79 @@
+#include "Lista.h"
+
+typedef Lista Conj;
+
+Conj vacio();
+Conj insert(Elem e, Conj c);
+bool esvacio(Conj);
+bool estaen(Elem, Conj);
+Conj elimina(Elem, Conj);
+Conj une(Conj, Conj);
+Conj interseccion(Conj, Conj);
+Conj diferencia(Conj, Conj);
+int cardinalidad(Conj);
+void impC(Conj);
+
+Conj vacio() {
+	return NULL;
+}
+
+bool esvacio(Conj c) {
+	return esvacia(c);
+}
+
+bool estaen(Elem e, Conj c) {
+	if (esvacio(c))
+		return false;
+	if (e == cabeza(c))
+		return true;
+
+	return estaen(e, resto(c));
+}
+
+Conj insert(Elem e, Conj c) {
+	return cons(e, c);
+}
+
+Conj elimina(Elem e, Conj c) {
+	if (esvacio(c))
+		return vacio();
+	if (e == cabeza(c))
+		return resto(c);
+
+	return cons(cabeza(c), elimina(e, resto(c)));
+}
+
+Conj une(Conj c1, Conj c2) {
+	if (esvacio(c2))
+		return c1;
+
+	return insert(cabeza(c2), une(c1, resto(c2)));
+}
+
+Conj interseccion(Conj c1, Conj c2) {
+	if (esvacio(c2))
+		return vacio();
+	if (estaen(cabeza(c2), c1))
+		return insert(cabeza(c2), interseccion(c1, resto(c2)));
+	return interseccion(c1, resto(c2));
+}
+
+Conj diferencia(Conj c1, Conj c2) {
+	if (esvacio(c2))
+		return c1;
+	if (estaen(cabeza(c2), c1))
+		return elimina(cabeza(c2), diferencia(c1, resto(c2)));
+	return diferencia(c1, resto(c2));
+}
+
+int cardinalidad(Conj c) {
+	if (esvacio(c))
+		return 0;
+
+	return 1 + cardinalidad(resto(c));
+}
+
+void impC(Conj c) {
+	impLista(c);
+}
+
